28db881c6aa95dc98569d2b92ca123dfd423e960
Deko911 <dmortizs2023@gmail.com>
2025-06-26 15:48:59 -0500

    Estrcutura y documentación

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+
+const fs = require('fs')
+const user = require('./controllers/user')
+const users = require('./models/users')
+
+// Motor de vistas para el template engine
+app.set('view engine', 'ejs')
+
+// Servimos los archivos estaticos
+app.use(express.static('public'))
+
+// Agregamos la rutas de la api
+app.use('/api/users', user)
+
+// Manejamos las rutas la pagina
+app.get('/', (req, res) => {
+  res.render('layout', { option: 'registro', title: 'Registro', users: null })
+})
+
+app.get('/:option', (req, res) => {
+  const option = req.params.option
+
+  const title = option.charAt(0).toUpperCase() + option.slice(1).toLowerCase()
+
+  let Dusers = null
+
+  if (fs.existsSync(`views/pages/${option}.ejs`)) {
+    if (option.toLowerCase() === 'inicio') {
+      Dusers = users
+    }
+    res.render('layout', { option, title, users: Dusers })
+  } else {
+    res.status(404)
+    return res.render('layout', { option: '404', title: 'Not found', users: null })
+  }
+})
+
+module.exports = app
