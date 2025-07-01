0001a7c29ec5d1b82a78c4b6c500ee45b5aecb49
Deko911 <dmortizs2023@gmail.com>
2025-06-30 23:22:17 -0500

    Frontend terminado

--- models/users.js
+++ models/users.js
@@ -1,10 +1,14 @@
 const users = [
   {
     name: 'Diego',
+    email: 'emaildediego@gmail.com',
+    date: '29/02/2026',
     password: 123
   },
   {
     name: 'Oscar',
+    email: 'emaildeoscar@gmail.com',
+    date: '29/02/2026',
     password: 456
   }
 ]
